1dfe3fac4789ea834c51f9fd846e17b6d1230387
triazur <guillaume.cassin@triazur.com>
2020-12-17 15:29:34 +0100

    first commit

---
+++ src/components/ToolTips/ToolTips.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Tooltip  } from 'reactstrap';
+import { FaInfoCircle } from 'react-icons/fa/';
+import './ToolTips.css';
+
+const ToolTips = (props) => {
+  const [status, setStatus] = useState(false);
+  return (
+    <div className="tooltips">
+      <div className="groupInfo" id={props.target}>
+        <FaInfoCircle className="icon" />
+      </div>
+      <Tooltip placement="top" isOpen={status} target={props.target} toggle={() => setStatus(!status)}>
+        {props.description}
+      </Tooltip>
+    </div>
+  );
+}
+
+export default ToolTips;
